accc90186c9c67f9d1e76d2f6470398936854de1
jeffxo419 <godlikejeff@gmail.com>
2020-10-12 18:08:16 +0800

    20201012

--- app/src/main/java/com/example/notificationandroid/MainActivity.kt
+++ app/src/main/java/com/example/notificationandroid/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.notificationandroid
 
 import android.annotation.SuppressLint
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,20 +8,42 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.media.RingtoneManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
 import com.example.swap.DateUtil
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.intentFor
 
+
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = Firebase.analytics
+
+
+
+
+
+
         bt1.setOnClickListener{
-            sendCounterNotify("Android","通知成功")
+            sendCounterNotify("Android", "通知成功")
+
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+                param(FirebaseAnalytics.Param.ITEM_ID, "id")
+                param(FirebaseAnalytics.Param.ITEM_NAME, "name")
+                param(FirebaseAnalytics.Param.CONTENT_TYPE, "type")
+            }
+
+
         }
     }
 
@@ -31,7 +52,12 @@ class MainActivity : AppCompatActivity() {
     private fun  sendCounterNotify(title: String, message: String) {
         val cancelIntent = intentFor<MainActivity>()
         val channel = NotificationChannel("1", "test", NotificationManager.IMPORTANCE_HIGH)
-        val piCancel = PendingIntent.getActivity(this, R.string.app_name, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val piCancel = PendingIntent.getActivity(
+            this,
+            R.string.app_name,
+            cancelIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
         val builder = NotificationCompat.Builder(this, "1")
 
 
@@ -53,9 +79,11 @@ class MainActivity : AppCompatActivity() {
                 .setLights(Color.RED, 1000, 1000) //燈
                 .setUsesChronometer(true)
 
-                .setStyle(NotificationCompat.BigPictureStyle()
-                    .bigPicture(BitmapFactory.decodeResource(this.resources, R.drawable.apple))
-                    .bigLargeIcon(bitmap))  //大圖通知
+                .setStyle(
+                    NotificationCompat.BigPictureStyle()
+                        .bigPicture(BitmapFactory.decodeResource(this.resources, R.drawable.apple))
+                        .bigLargeIcon(bitmap)
+                )  //大圖通知
                  //.addAction(R.drawable.ic_launcher_background,"取消",piCancel)
 
                 .setSubText("${DateUtil.nowDateTime}")
